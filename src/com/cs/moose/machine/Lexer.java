82305c84f76076fe8e3f878d841cba981b16dfce
Michael Neu <jytho@web.de>
2015-05-07 17:52:26 +0200

    Fixes fix of #11

--- src/com/cs/moose/machine/Lexer.java
+++ src/com/cs/moose/machine/Lexer.java
@@ -36,7 +36,7 @@ public class Lexer {
 	public static String stripNonCommands(String code) {
 		code = code.replaceAll("[A-Za-z0-9\\-]*\\:", "");
 		code = code.replaceAll(regexComments, "");
-		code = code.replaceAll("\\h+", " ");
+		code = code.replaceAll("\\h+", "");
 		
 		return code;
 	}
